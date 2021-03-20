7753a1367145ef61efe33cca83ae8f70010ea92d
Andreas Kroll <andreas.kroll@yahoo.de>
2021-03-20 19:58:14 +0100

    create dca langugae for missing labels in tl_settings
    https://github.com/thescrat/contao-loginlink/blob/02621f6d80c50ef3390958419bb86a318bcf23c6/src/Resources/contao/dca/tl_settings.php#L25-L38

---
+++ src/Resources/contao/languages/de/tl_settings.php
@@ -0,0 +1,14 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_settings']['login_link_legend'] = 'Login-Link Einstellungen';
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_settings']['login_link_autoKey'] 	= 'Login-Link Auto-Key';
+$GLOBALS['TL_LANG']['tl_settings']['login_link_defaultKeyLength'] 	= 'Login-Link standart Key-Länge';
